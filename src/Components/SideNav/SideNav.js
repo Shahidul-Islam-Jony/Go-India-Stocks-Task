dfd73032a28face874a5efbfff1285b2241ce0cc
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-03-22 23:33:10 +0600

    Side nav design done

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -1,5 +1,7 @@
 import { IoPersonSharp, IoNotifications } from "react-icons/io5";
 import { FaCaretDown } from "react-icons/fa6";
+import { TbArrowUpRight } from "react-icons/tb";
+import { BiSolidDollarCircle } from "react-icons/bi";
 import { GoDotFill } from "react-icons/go";
 import { MdChat } from "react-icons/md";
 import Link from "next/link";
@@ -16,10 +18,24 @@ const SideNav = () => {
             <GoDotFill className="absolute text-red-500 right-0" />
           </div>
         </div>
-        <div className="p-4">
-          <Link href="#" className="flex items-center gap-2">
-            <MdChat className="text-2xl" /> Disscussion Form <FaCaretDown className="ml-10 md:ml-20"/>
+        <div className="flex flex-col p-4">
+          <Link href="#" className="flex items-center gap-2 mb-4">
+            <MdChat className="md:text-xl" /> Disscussion Form
+            <FaCaretDown className="" />
           </Link>
+          <Link href="#" className="flex items-center gap-2 mb-4">
+            <span className="relative flex">
+              <BiSolidDollarCircle className="md:text-lg"/>
+              <TbArrowUpRight className="absolute left-2 bottom-2"/>
+            </span>
+            Market Stories
+          </Link>
+          <Link href="#" className="mb-4 pl-6">Sentiment</Link>
+          <Link href="#" className="mb-4 pl-6">Market</Link>
+          <Link href="#" className="mb-4 pl-6">Sector</Link>
+          <Link href="#" className="mb-4 pl-6">Watchlist</Link>
+          <Link href="#" className="mb-4 pl-6">Events</Link>
+          <Link href="#" className="mb-4 pl-6">News/Interview</Link>
         </div>
       </div>
     </div>
